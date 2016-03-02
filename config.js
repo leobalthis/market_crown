98737853fceb0d80e218fa5f8d8a402f1cd04ef2
gen4 <valentin.lapchevskiy@onetwotrip.com>
2016-03-02 13:46:44 +0300

    mongo conf

--- config.js
+++ config.js
@@ -2,10 +2,10 @@
 
 var config = {
 	SESSION_MONGO:{
-		URL:'mongodb://mongo/mcm-sessions'
+		URL:'mongodb://localhost/mcm-sessions'
 	},
 	USER_MONGO:{
-		URL:'mongodb://mongo/mcm-users'
+		URL:'mongodb://localhost/mcm-users'
 	},
 	PUBLIC_ADDRESS:'http://104.239.245.54',
 	PORT:3848
