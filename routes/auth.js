0edeaec2550a2ed4cd82f09c3914d9d53fa798fe
gen4 <valentin.lapchevskiy@onetwotrip.com>
2016-03-03 21:50:42 +0300

    bug fix

--- routes/auth.js
+++ routes/auth.js
@@ -128,11 +128,11 @@ router.post('/finish',urlencodedParser, function(req,res){
 			if(body=="User successfully added"){
 				User.deleteAllNonfinished();
 				req.user.saveMcUsername(req.body.user_name,function(err){
-					res.json({redirect:CONFIG.REDIRECT_URL_AFTER_SUCCESS_SIGNUP});
+					return res.json({redirect:CONFIG.REDIRECT_URL_AFTER_SUCCESS_SIGNUP});
 					//return res.redirect(CONFIG.REDIRECT_URL_AFTER_SUCCESS_SIGNUP);
 				});
 			}
-			res.json({error:body})
+			return res.json({error:body})
 	});
 });
 module.exports = router;
