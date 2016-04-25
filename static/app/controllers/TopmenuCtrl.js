c5bb59ce3e3417857dad3b4da56a2d716bebcdce
gen4 <gen4mail@gmail.com>
2016-04-26 00:53:37 +0300

    profile and fix

--- static/app/controllers/TopmenuCtrl.js
+++ static/app/controllers/TopmenuCtrl.js
@@ -1,6 +1,17 @@
 App.controller ('TopmenuCtrl',['$scope', '$http', 'MicroblogsService', 'GeneralDataService', 'UserDetailsService', function ($scope, $http, MicroblogsService, GeneralDataService,UserDetailsService){
-	var currentUsername = UserDetailsService.getUser().mc_username;
-	$scope.currentUsername=currentUsername;
-	console.log('currentUser',currentUsername);
+	var user = UserDetailsService.getUser(true);
+	$scope.currentUsername='';
+	if(user.then){
+		user.then(function(data){
+			console.log('then',data);
+			$scope.currentUsername = data.mc_username;
+		})
+	}else if(user.mc_username){
+		console.log('then mc_username',user);
+		$scope.currentUsername = user.mc_username;
+	}else{
+		console.log('then -- ')
+		$scope.currentUsername = ''
+	};
 
 }]);
